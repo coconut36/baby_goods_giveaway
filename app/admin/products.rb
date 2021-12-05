5c065a359b93b18bfec2b12751f66449a598513f
anonymous anonymous <coconut3691@gmail.com>
2021-12-05 22:52:54 +0000

    Whitelist columns in Product for ActiveAdmin

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register Product do
 
+ permit_params :giver_id, :name, :description, :age_id, :category_id, :gender, :image, :location
+
   # See permitted parameters documentation:
   # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
   #
