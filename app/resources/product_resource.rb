02902c2a22a8c5617ddba4075705a093958629e0
anonymous anonymous <coconut3691@gmail.com>
2021-12-05 22:53:36 +0000

    Generate graphiti resources for products

---
+++ app/resources/product_resource.rb
@@ -0,0 +1,18 @@
+class ProductResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :giver_id, :integer
+  attribute :name, :string
+  attribute :description, :string
+  attribute :age_id, :integer
+  attribute :category_id, :integer
+  attribute :gender, :string_enum, allow: Product.genders.keys
+  attribute :image, :string
+  attribute :location, :string
+
+  # Direct associations
+
+  # Indirect associations
+
+end
